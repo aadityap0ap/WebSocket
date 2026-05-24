f0fc3a8b806a95d40c63562bf591f28def841205
aadityap0ap <adityabharti450ap@gmail.com>
2026-05-24 18:23:25 +0530

    A basic websocket Server created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hii");
+import {WebSocketServer} from 'ws';
+const wss = new WebSocketServer({port:8080});
+
+wss.on("connection",function(socket){
+    console.log("your persitent connection established!");
+    socket.send("hello");
+})
