bda9f3d9a9d32c7e625bf8c59cf695512efd175e
aadityap0ap <adityabharti450ap@gmail.com>
2026-05-25 16:24:55 +0530

    Some Changes done to make it applicable to run with demo frontend app

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,10 @@ const wss = new WebSocketServer({port:8080});
 
 wss.on("connection",function(socket){
     console.log("your persitent connection established!");
-    socket.send("hello");
+    
+    socket.on("message" ,(e) => {
+        if(e.toString() === "ping"){
+            socket.send("pong");
+        }
+    })
 })
